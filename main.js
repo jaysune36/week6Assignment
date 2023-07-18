bedb441d5d2042e3de1daa7131208edfaaea3d45
user <user@users-MacBook-Pro.local>
2023-07-17 20:37:07 -0700

    created the shuffleDeck method within the game class. This method will give each player that is created an array of numbers and letters totaling 26(half of the deck class array). Working on startGame function to start the game when selected

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ class Cards {
       spades: []
     }
   }
-// this createCards method when called will push to each card type array and add the associated cards and there suits to each array through a loop
+  // this createCards method when called will push to each card type array and add the associated cards and there suits to each array through a loop
   createCards() {
     for (let i = 0; i < 4; i++) {
       if (this.cardTypes.hearts.length != 13) {
@@ -38,37 +38,42 @@ class Cards {
 }
 
 class Players {
-  constructor() {
-    this.playerOne = [];
-    this.playerTwo = [];
+  constructor(name) {
+    this.name = name;
+    this.hand = [];
+  }
+
+  describe() {
+    return `${this.name} has ${this.hand.length} in their hand. Here are there cards
+    ${this.hand}
+    `
   }
 }
 
 class Deck {
   constructor() {
-    this.deck = [];
+    this.cardsInDeck = [];
   }
 
   // the addCards methods will create a new cards varialbe using the new Cards class. Then will call the createCards method to the cards variable and push the newly created cards to the this.deck property and return the property
   addCards() {
     let cards = new Cards();
     cards.createCards();
-    this.deck = [...cards.cardTypes.hearts, ...cards.cardTypes.clubs, ...cards.cardTypes.diamonds, ...cards.cardTypes.spades];
-    return this.deck;
-  }
-
-  shuffleDeck() {
-    this.addCards();
+    this.cardsInDeck = [...cards.cardTypes.hearts, ...cards.cardTypes.clubs, ...cards.cardTypes.diamonds, ...cards.cardTypes.spades];
+    return this.cardsInDeck;
   }
 }
 
 class Game {
+  constructor() {
+    this.players = [];
+  }
 
   // the start method will initalize the mainMenu mehtod at the time the browser starts. It will then display the mainMenu prompt and allow the user to start the game or exit. If the user decides to exit an alert will display.
   start() {
     let select = this.mainMenu();
     if (parseInt(select) === 1) {
-      return this.mainMenu()
+      console.log('the game has started');
     } else {
       alert('Thank you for playing!')
     }
@@ -84,11 +89,33 @@ class Game {
     `)
   }
 
+  startGame() {
+    this.players.push(new Players('Player One'));
+    this.players.push(new Players('Player Two'));
+    this.shuffleDeck()
+    return this.players
+  }
+
+// the shuffleDeck method will create a new Deck using the new Deck() class and then using a for loop randomly push a card to player[0](or Player One)'s hand. The remaining cards will then be pushed to player 2's hand array. 
+  shuffleDeck() {
+    let deck = new Deck();
+    deck.addCards();
+    for (let i = 0; i < deck.cardsInDeck.length; i++) {
+      let randomNumber = Math.floor(Math.random() * deck.cardsInDeck.length);
+      let randomCard = deck.cardsInDeck[randomNumber];
+      this.players[0].hand.push(randomCard);
+      deck.cardsInDeck.splice(randomNumber, 1);
+      if(this.players[0].hand.length === 26) {
+        break;
+      }
+    }
+    this.players[1].hand.push(...deck.cardsInDeck)
+  }
 }
 
 
 
-// //this variable creates the menu class
+//this variable creates the menu class
 // let game = new Game();
 // //After calling the start method this will initalize the main menu prompt
 // game.start()
