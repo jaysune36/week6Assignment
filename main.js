3a89d5fbb9776c0e0d44b1cfd6bf12db36b0708d
user <user@users-MacBook-Pro.local>
2023-07-17 17:58:55 -0700

    First commit for WAR game emulator

---
+++ main.js
@@ -0,0 +1,23 @@
+class Cards {
+
+}
+
+class Players {
+
+}
+
+class Deck {
+
+}
+
+class Menu {
+
+  mainMenu() {
+    return `
+    Hello and welcome to WAR!
+    Please select an option
+    0) Exit
+    1) Who wins Player 1 or Player 2
+    `
+  }
+}
